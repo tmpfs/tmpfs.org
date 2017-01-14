1cc0fd840fb8c8262f3c989e2339ecbd92a49b5a
muji <freeformsystems@gmail.com>
2017-01-14 08:26:32 +0800

    Initial files.

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HardSourcePlugin = require('hard-source-webpack-plugin')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
+const postCssSimpleVars = require('postcss-simple-vars')
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
+  reshape: (ctx) => {
+    return htmlStandards({
+      webpack: ctx,
+      locals: { pageId: pageId(ctx) }
+    })
+  },
+  postcss: (ctx) => {
+    const css = cssStandards({ webpack: ctx });
+    css.plugins.push(postCssSimpleVars());
+    return css;
+  },
+  babel: { presets: [jsStandards] },
+  plugins: [
+    new HardSourcePlugin({
+      environmentPaths: { root: __dirname },
+      recordsPath: path.join(__dirname, '_cache/records.json'),
+      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
+    })
+  ]
+}
