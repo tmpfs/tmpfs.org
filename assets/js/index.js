b5cf35629357fdfbfcb833cd41188b51fe06278c
muji <freeformsystems@gmail.com>
2017-01-15 17:26:01 +0800

    Selected state for main nav.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,36 @@
-// console.log('you can use ES6 here : )')
+const HOME_ID = 'index';
+
+class Application {
+
+  constructor() {
+    // main element
+    this.main = document.querySelector('main');
+    this.nav = document.querySelector('header > nav');
+
+    // page id
+    this.page = this.main.getAttribute('id');
+  }
+
+  setSelectedLink() {
+    if(this.page !== HOME_ID) {
+      const link = this.nav.querySelector('.' + this.page);
+
+      if(!link) {
+        throw new Error(
+          `could not locate link for id '${this.page}', sync nav class names?`);
+      }
+
+      link.classList.add('selected');
+
+      console.log(link.classList);
+    }
+  }
+
+  start() {
+    this.setSelectedLink();
+  }
+
+}
+
+const app = new Application();
+app.start();
