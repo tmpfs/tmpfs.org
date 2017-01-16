fe97507ac8d9a20fc5695024fab16d2cd3014490
muji <freeformsystems@gmail.com>
2017-01-16 23:28:38 +0800

    Basic progressive enhancement.

--- assets/js/progressive.js
+++ assets/js/progressive.js
@@ -6,14 +6,69 @@ class Progressive {
 
   constructor() {
     this.links = Array.from(document.querySelectorAll('header a'));
+    this.click = this.onClick.bind(this);
+  }
+
+  fallback(href) {
+    document.location.pathname = href;
+  }
+
+  render(href) {
+    fetch(href).then(function(response) {
+      if(response.ok) {
+        response.text().then(function(doc) {
+          const html = document.createElement('html');
+          html.innerHTML = doc;
+          const main = html.querySelector('main');
+          console.log(main);
+          const current = document.querySelector('main');
+          current.parentNode.replaceChild(main, current);
+        })
+      }else{
+        console.log('Network response was not ok.');
+        this.fallback(href);
+      }
+    })
+    .catch(function(error) {
+      console.log(`fetch error: ${error.message}`);
+      this.fallback(href);
+    });
+  }
+
+  onClick(evt) {
+    evt.preventDefault();
+
+    const link = evt.currentTarget
+        , pathname = document.location.pathname.replace(/\/$/, '');
+
+
+    let href = link.getAttribute('href').replace(/\/$/, '')
+
+    // attempt to navigate to current path
+    if(href === pathname) {
+      return false; 
+    }
+
+    console.log(document.location);
+    console.log(href);
+
+    if(href === '') {
+      href = '/';
+    }
+
+    history.pushState({}, '', href);
+
+    this.render(href);
   }
 
   start() {
-    console.log('start progressive enhancement');
+    if(window.fetch !== null) {
+      this.links.forEach((link) => {
+        link.addEventListener('click', this.click);
+      })
 
-    this.links.forEach((link) => {
-      console.log(link) 
-    })
+      //wid
+    }
   }
 
 }
