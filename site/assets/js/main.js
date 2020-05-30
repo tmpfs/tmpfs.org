c3b76b78ac76644708cadda66c112412314baa62
muji <muji@tmpfs.org>
2020-05-30 14:29:12 +0800

    Remove contrast related javascript.

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,42 +1,16 @@
 import {Selected} from './selected.js';
 import {Scroll} from './scroll.js';
-import {Contrast} from './contrast.js';
 import {Progressive} from './progressive.js';
 
 class Application {
 
   constructor() {
-    // TODO: redirect to unsupported browser on feature check
-    this.contrast = new Contrast();
     this.selected = new Selected();
     this.scroller = new Scroll();
     this.progressive = new Progressive(this.selected);
   }
 
-  isIos() {
-    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  }
-
-  isIos4() {
-    return !!window.history && ('pushState' in window.history);
-  }
-
-  isIos5() {
-    return !!window.matchMedia;
-  }
-
   start() {
-
-    // animation redraw issues changing contrast on old iphone
-    // so disable the contrast toggle
-    if(this.isIos() && (this.isIos5() || this.isIos4())) {
-      document.querySelector('ul.workflow li')
-        .setAttribute('style', 'display: inline-block')
-      this.contrast.disable();
-    }else{
-      this.contrast.start();
-    }
-
     this.selected.start();
     this.scroller.start();
     this.progressive.start();
