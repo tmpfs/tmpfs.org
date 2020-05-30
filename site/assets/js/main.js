a83a6b9abecfb657895c2bbfe4861f176066c187
muji <muji@tmpfs.org>
2020-05-30 14:42:49 +0800

    Remove progressive loading.

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,15 +1,12 @@
 import {Selected} from './selected.js';
-import {Progressive} from './progressive.js';
 
 class Application {
   constructor() {
     this.selected = new Selected();
-    this.progressive = new Progressive(this.selected);
   }
 
   start() {
     this.selected.start();
-    this.progressive.start();
   }
 }
 
