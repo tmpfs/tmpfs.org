ffcfa4cdd449c893b5738ad6bacfcb68f41787a2
muji <muji@tmpfs.org>
2020-05-30 14:37:10 +0800

    Remove smooth scroll.

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,21 +1,16 @@
 import {Selected} from './selected.js';
-import {Scroll} from './scroll.js';
 import {Progressive} from './progressive.js';
 
 class Application {
-
   constructor() {
     this.selected = new Selected();
-    this.scroller = new Scroll();
     this.progressive = new Progressive(this.selected);
   }
 
   start() {
     this.selected.start();
-    this.scroller.start();
     this.progressive.start();
   }
-
 }
 
 const app = new Application();
